af3d21a4aa44d65b5c1aea5a7f503a9ee30b3ea3
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-01-24 15:37:51 +0500

    chore: enhanced, turned into more pure tailwind-way of handling background for better customization

--- src/components/HeaderPart.vue
+++ src/components/HeaderPart.vue
@@ -1,33 +1,23 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import { defineProps } from 'vue'
-
 import img from '@/assets/pics/iStock-1164049276-min_RgsJ0rS.jpg'
 
-let props = defineProps(['title', 'customTemp'])
-
-let header = ref(null)
-
-onMounted(() => {
-  header.value.style = `
-        background-image: url(${img});
-        background-size: cover;    
-        background-position: center left;
-    `
-})
+let props = defineProps(['title'])
 </script>
 
 <template>
-  <div
-    ref="header"
-    class="z-10 h-[40vh] before:absolute before:-z-10 before:w-full before:h-full before:bg-[rgba(0,0,0,0.6)] relative flex justify-center items-center flex-col text-white"
-  >
-    <div class="text-center" v-if="!props.customTemp">
-      <h1 class="text-4xl font-semibold tracking-wide">{{ props.title }}</h1>
-      <p class="text-xl">
-        <router-link :to="{ name: 'home' }">Home</router-link> / {{ props.title }}
-      </p>
-    </div>
-    <slot v-if="props.customTemp"></slot>
+  <div ref="header" :style="{
+    '--bg-image-url': `url(${img})`
+  }"
+    class="z-10 bg-cover bg-[image:--bg-image-url] bg-[position:left_center] h-[40vh] before:absolute before:-z-10 before:w-full before:h-full before:bg-[rgba(0,0,0,0.6)] relative flex justify-center items-center flex-col text-white">
+    <!-- giving default template for slot.
+          gets replaced by given slot content -->
+    <slot>
+      <div class="text-center">
+        <h1 class="text-4xl font-semibold tracking-wide">{{ props.title }}</h1>
+        <p class="text-xl">
+          <router-link :to="{ name: 'home' }">Home</router-link> / {{ props.title }}
+        </p>
+      </div>
+    </slot>
   </div>
 </template>
